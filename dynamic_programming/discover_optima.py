ddad2e9e425af1da28ebd4a2629e428f8ac1b7a7
Lars <l.w.vanblokland@student.tudelft.nl>
2022-10-04 11:20:14 +0200

    Finalized non-citylearn evaluation

--- dynamic_programming/discover_optima.py
+++ dynamic_programming/discover_optima.py
@@ -44,14 +44,69 @@
 import numpy as np
 
 
+def evaluate(net_electricity_consumption, net_electricity_consumption_without_storage, electricity_pricing,
+             carbon_intensity):
+    net_electricity_consumption = np.asarray(net_electricity_consumption)
+    net_electricity_consumption_without_storage = np.asarray(net_electricity_consumption_without_storage)
+    electricity_pricing = np.asarray(electricity_pricing)
+    carbon_intensity = np.asarray(carbon_intensity)
+
+    net_electricity_consumption_price = net_electricity_consumption * electricity_pricing
+    net_electricity_consumption_price_without_storage = net_electricity_consumption_without_storage * electricity_pricing
+
+    net_electricity_consumption_emission = net_electricity_consumption * carbon_intensity
+    net_electricity_consumption_emission_without_storage = net_electricity_consumption_without_storage * carbon_intensity
+
+    price_score = price(net_electricity_consumption_price) / price(net_electricity_consumption_price_without_storage)
+    carbon_score = carbon(net_electricity_consumption_emission) / carbon(
+        net_electricity_consumption_emission_without_storage)
+
+    load_factor_score = load_factor(net_electricity_consumption) / load_factor(
+        net_electricity_consumption_without_storage)
+    ramping_score = ramping(net_electricity_consumption) / ramping(net_electricity_consumption_without_storage)
+
+    grid_score = (load_factor_score + ramping_score) / 2
+
+    return price_score, carbon_score, grid_score
+
+
 def price(net_electricity_consumption_price_list):
     net_electricity_consumption_price_list = np.asarray(net_electricity_consumption_price_list)
     return np.cumsum(np.clip(net_electricity_consumption_price_list, a_min=0, a_max=None))[-1]
+
 
 def carbon(net_electricity_consumption_carbon_list):
     net_electricity_consumption_carbon_list = np.asarray(net_electricity_consumption_carbon_list)
     return np.cumsum(np.clip(net_electricity_consumption_carbon_list, a_min=0, a_max=None))[-1]
 
-def ramping(net_electricity_consumption):
 
-    pass
+def ramping(net_electricity_consumption):
+    net_electricity_consumption = np.asarray(net_electricity_consumption)
+
+    shifted_net_electricity_consumption = np.roll(net_electricity_consumption, 1)
+    shifted_net_electricity_consumption[0] = 0
+
+    subtracted_net_electricity_consumption = np.abs(net_electricity_consumption - shifted_net_electricity_consumption)
+    subtracted_net_electricity_consumption[0] = 0
+
+    return np.cumsum(subtracted_net_electricity_consumption)[-1]
+
+
+def load_factor(net_electricity_consumption):
+    net_electricity_consumption = np.asarray(net_electricity_consumption)
+
+    window_size = 730
+
+    windowed_net_electricity_consumption = np.split(net_electricity_consumption,
+                                                    np.arange(window_size, len(net_electricity_consumption),
+                                                              window_size))
+
+    calculation_list = []
+
+    for window in windowed_net_electricity_consumption:
+        window_max = window.max()
+        window_mean = window.mean()
+
+        calculation_list.append(1 - window_mean / window_max)
+
+    return np.asarray(calculation_list).mean()
