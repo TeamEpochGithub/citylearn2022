cf6655d933077bfb27008e9c0f25c0ac121f712b
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-05 17:47:02 +0200

    remove unused import from user agent

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,14 +1,5 @@
-from agents.month_tunable_agent import TunableMonthAgent
-from agents.random_agent import RandomAgent
-from agents.rbc_agent import BasicRBCAgent
-from agents.basic_reward_agent import BasicRewardAgent
-from agents.weather_agent import BasicWeatherAgent
-from agents.sac_agent import SAC
-from agents.rlc_agent import RLCAgent
-from agents.spinning_up_agent import BasicPPOAgent
 from agents.month_tuned_agent import MonthTunedAgent
-from agents.day_tuned_agent import DayTunedAgent
-from agents.month_tunable_agent import TunableMonthAgent
+
 
 ###################################################################
 #####                Specify your agent here                  #####
