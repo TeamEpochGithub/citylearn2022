8262be3a58d741fb4958a0ff82d3acf968031b7b
Adrian <kuiper2000@hotmail.com>
2022-10-06 23:41:24 +0200

    load_factor model

--- consumption_prediction/predict.py
+++ consumption_prediction/predict.py
@@ -11,10 +11,10 @@ def predict():
 
     pipeline_optimizer = TPOTRegressor()
 
-    solar_df = pd.read_csv(r"C:\Users\Lars\Documents\Epoch\CityLearn\citylearn-2022-starter-kit\consumption_prediction\solar_data.csv")
+    solar_df = pd.read_csv(r"C:\Users\kuipe\OneDrive\Bureaublad\Epoch\citylearn-2022-starter-kit\consumption_prediction\load_data.csv")
 
-    solar_df_data = solar_df.drop(["solar_generation_future"], axis=1)
-    solar_df_target = solar_df["solar_generation_future"]
+    solar_df_data = solar_df.drop(["non_shiftable_load_future"], axis=1)
+    solar_df_target = solar_df["non_shiftable_load_future"]
 
     X_train, X_test, y_train, y_test = train_test_split(solar_df_data.to_numpy(), solar_df_target.to_numpy(),
                                                         train_size=0.8, test_size=0.2)
@@ -24,7 +24,7 @@ def predict():
 
     pipeline_optimizer.fit(X_train, y_train)
     print(pipeline_optimizer.score(X_test, y_test))
-    pipeline_optimizer.export('tpot_exported_pipeline_solar_1.py')
+    pipeline_optimizer.export('tpot_exported_pipeline_load_1.py')
 
 
 if __name__ == '__main__':
