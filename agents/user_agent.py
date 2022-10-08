028a6b07ca876689470bf3c600c1f49e1dcb69a6
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-08 12:43:33 +0200

    add prediction-based agent

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,5 +1,6 @@
 from agents.combined_policy_agent import MultiPolicyAgent
-from agents.consumption_based_agent import ConsumptionBasedAgent, ConsumptionPredAgent
+from agents.consumption_based_agent import ConsumptionBasedAgent
+from agents.consumption_pred_agent import ConsumptionPredAgent
 from agents.month_tuned_agent import MonthTunedAgent
 from agents.month_tuned_agent_nonranges import MonthTunedAgentNoRanges
 
