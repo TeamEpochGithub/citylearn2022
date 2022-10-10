cba648489f96e2747ece3bc87389ce847e610e97
Adrian <a.s.kuiper@student.tudelft.nl>
2022-10-10 11:49:58 +0200

    load factor predict and solar model and timeseries test

--- consumption_prediction/predict.py
+++ consumption_prediction/predict.py
@@ -19,8 +19,8 @@ def predict():
     X_train, X_test, y_train, y_test = train_test_split(solar_df_data.to_numpy(), solar_df_target.to_numpy(),
                                                         train_size=0.8, test_size=0.2)
 
-    pipeline_optimizer = TPOTRegressor(generations=5, population_size=20, cv=5,
-                                        random_state=42, verbosity=2)
+    pipeline_optimizer = TPOTRegressor(generations=100, population_size=100, cv=5,
+                                        random_state=42, verbosity=2, n_jobs=-1)
 
     pipeline_optimizer.fit(X_train, y_train)
     print(pipeline_optimizer.score(X_test, y_test))
