c58072ce6745177825242955973d519cd83b8ade
Lars <l.w.vanblokland@student.tudelft.nl>
2022-10-11 16:13:33 +0200

    Now predicts load factor correctly

--- consumption_prediction/lstm_load_prediction.py
+++ consumption_prediction/lstm_load_prediction.py
@@ -17,6 +17,7 @@ from data import citylearn_challenge_2022_phase_1 as competition_data
 from sklearn.linear_model import Ridge
 from lightgbm import LGBMRegressor
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error
 from skforecast.ForecasterAutoreg import ForecasterAutoreg
 from skforecast.ForecasterAutoregDirect import ForecasterAutoregDirect
@@ -210,16 +211,18 @@ def average_difference():
     print(mse_b5)
 
 
-def auto_regressor_test():
+def auto_regressor_test_non_grid():
     load_df = pd.read_csv(
         r"C:\Users\Lars\Documents\Epoch\CityLearn\citylearn-2022-starter-kit\consumption_prediction\load_data.csv")
+
+    load_df[load_df.columns] = MinMaxScaler().fit_transform(load_df[load_df.columns])
 
     load_df_data = load_df.drop(["non_shiftable_load_future"], axis=1)
     load_df_target = load_df["non_shiftable_load_future"]
 
     total_records = 8758
 
-    test_fraction = 0.9
+    test_fraction = 0.8
     test_count = round(8758 * test_fraction)
 
     validation_fraction = 0.7
@@ -234,7 +237,84 @@ def auto_regressor_test():
 
         forecaster = ForecasterAutoreg(
             regressor=Ridge(random_state=42),
-            lags=100,
+            lags=[1,2,3,4,5,23,24,25,26,27, 48, 49, 50, 51, 52],
+            transformer_y=StandardScaler()
+        )
+
+        lags_grid = [[1,2,3,4,5], [1,2,3,23,24,25], [1,2,3,4,5,23,24,25,26,27], 5, 10, 15, 20, 25, [1,2,3,23,24,25, 48, 49, 50], [1,2,3,4,5,23,24,25,26,27, 48, 49, 50, 51, 52]]
+
+        param_grid = {'alpha': np.logspace(-3, 5, 10)}
+
+        # results_grid = grid_search_forecaster(
+        #     forecaster=forecaster,
+        #     y=data,
+        #     param_grid=param_grid,
+        #     lags_grid=lags_grid,
+        #     steps=24,
+        #     metric='mean_squared_error',
+        #     refit=False,
+        #     initial_train_size=len(data_train_val),
+        #     fixed_train_size=False,
+        #     return_best=True,
+        #     verbose=False
+        # )
+
+        forecaster.fit(data_train_test)
+        #
+        metric, predictions = backtesting_forecaster(
+            forecaster=forecaster,
+            y=data,
+            initial_train_size=len(data_train_test),
+            fixed_train_size=False,
+            steps=24,
+            metric="mean_squared_error",
+            refit=False,
+            verbose=False
+        )
+
+        metric_list.append(metric)
+        # grid_list.append(results_grid)
+
+        print(f"------House {index+1}-----")
+        print(f'Backtest error: {metric}')
+
+        fig, ax = plt.subplots(figsize=(12, 3.5))
+        data.loc[predictions.index].plot(ax=ax, linewidth=2, label='real')
+        predictions.plot(linewidth=2, label='prediction', ax=ax)
+        ax.set_title(f'Prediction vs real non shiftable load of house {index+1}')
+        ax.legend()
+
+        plt.show()
+
+    print(metric_list)
+
+def auto_regressor_test_grid():
+    load_df = pd.read_csv(
+        r"C:\Users\Lars\Documents\Epoch\CityLearn\citylearn-2022-starter-kit\consumption_prediction\load_data.csv")
+
+    load_df[load_df.columns] = MinMaxScaler().fit_transform(load_df[load_df.columns])
+
+    load_df_data = load_df.drop(["non_shiftable_load_future"], axis=1)
+    load_df_target = load_df["non_shiftable_load_future"]
+
+    total_records = 8758
+
+    test_fraction = 0.90
+    test_count = round(8758 * test_fraction)
+
+    validation_fraction = 0.7
+    validation_count = round(8758 * validation_fraction)
+
+    metric_list = []
+    grid_list = []
+
+    for index, data in enumerate(np.split(load_df_target, 5)):
+        data_train_val = data.iloc[:validation_count]
+        data_train_test = data.iloc[:test_count]
+
+        forecaster = ForecasterAutoreg(
+            regressor=Ridge(random_state=42),
+            lags=[1,2,3,23,24,25, 48, 49, 50],
             transformer_y=StandardScaler()
         )
 
@@ -256,8 +336,8 @@ def auto_regressor_test():
             verbose=False
         )
 
-        # forecaster.fit(data_train)
-        #
+        # forecaster.fit(data_train_test)
+        # #
         metric, predictions = backtesting_forecaster(
             forecaster=forecaster,
             y=data,
@@ -270,7 +350,7 @@ def auto_regressor_test():
         )
 
         metric_list.append(metric)
-        grid_list.append(results_grid)
+        # grid_list.append(results_grid)
 
         print(f"------House {index+1}-----")
         print(f'Backtest error: {metric}')
@@ -290,4 +370,5 @@ if __name__ == '__main__':
     # plot_average_lf()
     # day_difference()
     # average_difference()
-    auto_regressor_test()
+    # auto_regressor_test_grid()
+    auto_regressor_test_non_grid()
