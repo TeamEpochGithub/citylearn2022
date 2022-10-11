03b3b7d5666efb511b9dc1348be0f27217350acf
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-12 00:44:43 +0200

    clean up user agent imports

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,9 +1,5 @@
-from agents.combined_policy_agent import MultiPolicyAgent
-from agents.consumption_based_agent import ConsumptionBasedAgent
 from agents.consumption_based_tpot_actions_agent import ConsumptionBasedTPOTActionsAgent
-from agents.consumption_pred_agent import ConsumptionPredAgent
-from agents.month_tuned_agent import MonthTunedAgent
-from agents.month_tuned_agent_nonranges import MonthTunedAgentNoRanges
+
 
 ###################################################################
 #####                Specify your agent here                  #####
