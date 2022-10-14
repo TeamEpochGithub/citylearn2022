3639a981b30221982fe0ecdfeae6974264844a10
Lars <l.w.vanblokland@student.tudelft.nl>
2022-10-14 14:09:00 +0200

    Now predicts load factor better again

--- consumption_prediction/lstm_load_prediction.py
+++ consumption_prediction/lstm_load_prediction.py
@@ -275,10 +275,14 @@ def auto_regressor_test_non_grid():
 
             encountered_data.append(current)
 
-            forecaster.fit(pd.Series(encountered_data))
+            if len(encountered_data) > 1500:
+                del encountered_data[0]
+
+            if index % 2 == 0:
+                forecaster.fit(pd.Series(encountered_data))
 
         mse = (np.square(np.asarray(predictions) - np.asarray(truth))).mean()
-        non_backtest_metric.append(mse)
+        non_backtest_metric.append(np.sqrt(mse))
 
         print("Custom mse:", mse)
 
@@ -388,9 +392,106 @@ def auto_regressor_test_grid():
     print(metric_list)
 
 
+def auto_regressor_test_generic_start():
+    load_df = pd.read_csv(
+        r"C:\Users\Lars\Documents\Epoch\CityLearn\citylearn-2022-starter-kit\data\citylearn_challenge_2022_phase_1\load_data.csv")
+
+    load_df[load_df.columns] = MinMaxScaler().fit_transform(load_df[load_df.columns])
+
+    load_df_data = load_df.drop(["non_shiftable_load_future"], axis=1)
+    load_df_target = load_df["non_shiftable_load_future"]
+
+    total_records = 8758
+
+    test_fraction = 0.2
+    test_count = round(8758 * test_fraction)
+
+    validation_fraction = 0.7
+    validation_count = round(8758 * validation_fraction)
+
+    metric_list = []
+    non_backtest_metric = []
+
+    for index, data in enumerate(np.split(load_df_target, 5)):
+
+        print(f"------House {index + 1}-----")
+
+        data_train_val = data.iloc[:validation_count]
+        data_train_test = data.iloc[:test_count]
+
+        lag_size = 60
+
+        data_minimal = data.iloc[:lag_size]
+
+        encountered_data = list(data_minimal)
+
+        forecaster = ForecasterAutoreg(
+            regressor=Ridge(random_state=42),
+            lags=[1,2,3,4,5,23,24,25,26,27, 48, 49, 50, 51, 52],
+            transformer_y=StandardScaler()
+        )
+
+        forecaster.fit(pd.Series(data_minimal.values))
+
+        predictions = []
+        truth = []
+
+        for inner_index, current in enumerate(data.iloc[lag_size:]):
+
+            if inner_index % 200 == 0:
+                print(inner_index)
+
+            current_prediction = forecaster.predict(steps=1)
+
+            predictions.append(current_prediction.iloc[0])
+
+            truth.append(current)
+
+            encountered_data.append(current)
+
+            # if len(encountered_data) > 1500:
+            #     del encountered_data[0]
+
+            # if index % 2 == 0:
+            forecaster.fit(pd.Series(encountered_data))
+
+        mse = (np.square(np.asarray(predictions) - np.asarray(truth))).mean()
+        non_backtest_metric.append(np.sqrt(mse))
+
+        print("Custom mse:", mse)
+
+        metric, predictions_backtest = backtesting_forecaster(
+            forecaster=forecaster,
+            y=data,
+            initial_train_size=len(data_train_test),
+            fixed_train_size=False,
+            steps=24,
+            metric="mean_squared_error",
+            refit=False,
+            verbose=False
+        )
+
+        metric_list.append(np.sqrt(metric))
+
+
+        print(f'Backtest error: {np.sqrt(metric)}')
+
+
+        fig, ax = plt.subplots(figsize=(12, 3.5))
+        pd.Series(truth).plot(ax=ax, linewidth=2, label='real')
+        pd.Series(np.asarray(predictions)).plot(linewidth=2, label='prediction', ax=ax)
+        ax.set_title(f'Prediction vs real non shiftable load of house {index+1}')
+        ax.legend()
+
+        plt.show()
+
+    print(metric_list)
+    print(non_backtest_metric)
+
 if __name__ == '__main__':
     # plot_average_lf()
     # day_difference()
     # average_difference()
     # auto_regressor_test_grid()
-    auto_regressor_test_non_grid()
+    # auto_regressor_test_non_grid()
+    auto_regressor_test_generic_start()
