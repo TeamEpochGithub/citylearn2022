88717376b2a0bdc7fa6c1e99b4f0dfa9fb9dba45
Björn <bjornbucksch16@gmail.com>
2022-10-14 17:58:46 +0200

    Imrpoved_individual_consumption

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,12 +1,12 @@
 #from agents.individual_consumption_agent import IndividualConsumptionAgent
-from agents.improved_individual_consumption import IndividualConsumptionAgent
+from agents.improved_individual_consumption import ImprovedIndividualConsumptionAgent
 
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
 
 # UserAgent = RandomAgent
-UserAgent = IndividualConsumptionAgent
+UserAgent = ImprovedIndividualConsumptionAgent
 # UserAgent = SAC(RLCAgent)
 # UserAgent = BasicRewardAgent
 # UserAgent = BasicWeatherAgent
