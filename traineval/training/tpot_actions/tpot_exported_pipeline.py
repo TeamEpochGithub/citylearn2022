e9306172812313eb62e33249ba475dddbd976366
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-15 19:17:28 +0200

    include best tpot pipeline

--- traineval/training/tpot_actions/tpot_exported_pipeline.py
+++ traineval/training/tpot_actions/tpot_exported_pipeline.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import RobustScaler
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +13,12 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: -0.00326979205124098
-exported_pipeline = ExtraTreesRegressor(bootstrap=False, max_features=0.9000000000000001, min_samples_leaf=5, min_samples_split=12, n_estimators=100)
+# Average CV score on the training set was: -0.003022221497420624
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=RidgeCV()),
+    RobustScaler(),
+    RandomForestRegressor(bootstrap=False, max_features=0.35000000000000003, min_samples_leaf=12, min_samples_split=18, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
