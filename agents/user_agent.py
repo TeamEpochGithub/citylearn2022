f1993c4eb5ccb7986a83f8ec14886fc0ab675305
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-18 20:16:28 +0200

    add (to be fixed) pred consumption agent

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -2,5 +2,6 @@
 #####                Specify your agent here                  #####
 ###################################################################
 from agents.known_consumption_agent import KnownConsumptionAgent
+from agents.pred_consumption_agent import PredConsumptionAgent
 
-UserAgent = KnownConsumptionAgent
+UserAgent = PredConsumptionAgent
