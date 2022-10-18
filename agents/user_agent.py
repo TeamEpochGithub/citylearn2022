7576c83649ada1c693b88ebf300f3f15da5d8231
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-18 19:23:56 +0200

    cleaning and refactoring

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,11 +1,6 @@
-from agents.clean_improved_individual_consumption import CleanImprovedIndividualConsumptionAgent
-from agents.cleaner_improved_individual_consumption import CleanerImprovedIndividualConsumptionAgent
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
-from agents.improved_individual_consumption import ImprovedIndividualConsumptionAgent
-from agents.improved_individual_consumption_live_learning import ImprovedIndividualConsumptionLiveLearningAgent
-from agents.live_learning_consumption_agent import LiveLearningConsumptionAgent
-from agents.live_learning_consumption_based_agent import LiveLearningAgentTPOTActions
+from agents.known_consumption_agent import KnownConsumptionAgent
 
-UserAgent = CleanImprovedIndividualConsumptionAgent
+UserAgent = KnownConsumptionAgent
