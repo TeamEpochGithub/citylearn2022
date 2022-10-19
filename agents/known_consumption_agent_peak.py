65e43512f33cf78b743fae6535ec750c7a824aa1
Adrian <a.s.kuiper@student.tudelft.nl>
2022-10-19 16:10:25 +0200

    known consumption agent with peaks

--- agents/known_consumption_agent_peak.py
+++ agents/known_consumption_agent_peak.py
@@ -8,14 +8,14 @@ from traineval.training.data_preprocessing import net_electricity_consumption as
 from traineval.training.data_preprocessing.pricing_simplified import pricing, shift_date
 
 consumptions_path = osp.join(osp.dirname(competition_data.__file__), "consumptions/building_consumptions.csv")
-# carbon_path = osp.join(osp.dirname(competition_data.__file__), "carbon_intensity.csv")
+carbon_path = osp.join(osp.dirname(competition_data.__file__), "carbon_intensity.csv")
 
 consumptions = pd.read_csv(consumptions_path)[[f"{i}" for i in range(5)]]
 consumptions = [consumptions[f"{i}"].values.tolist()[1:] for i in range(5)]
 
 
-# carbon = pd.read_csv(carbon_path)["kg_CO2/kWh"]
-# carbon = carbon.values.tolist()[1:]
+carbon = pd.read_csv(carbon_path)["kg_CO2/kWh"]
+carbon = carbon.values.tolist()[1:]
 
 def get_chunk_consumptions(agent_id, timestep, consumption_sign):
     chunk_consumptions = []
@@ -29,7 +29,6 @@ def get_chunk_consumptions(agent_id, timestep, consumption_sign):
         if timestep + future_steps >= 8759:
             break
 
-    print(chunk_consumptions)
     return chunk_consumptions
 
 
@@ -46,13 +45,74 @@ def negative_consumption_scenario(chunk_consumptions, remaining_battery_capacity
     return chunk_charge_loads
 
 
-def calculate_next_chunk(prev_consumption_sign, agent_id, timestep, remaining_battery_capacity, soc):
-    consumption_sign = -prev_consumption_sign
+def positive_consumption_scenario(observation, chunk_consumptions, timestep, remaining_battery_capacity, soc):
+    chunk_total_consumption = sum(chunk_consumptions)
+
+    date = shift_date(observation[2], observation[1], observation[0], shifts=1)
+
+    prices = []
+    emissions = []
+
+    for hour in range(len(chunk_consumptions)):
+        prices.append(pricing(date[2], date[0], date[1]))
+        date = shift_date(date[0], date[1], date[2], shifts=1)
+
+        emissions.append(carbon[timestep + hour])
+
+    consumption_prices = [prices[i] * c for i, c in enumerate(chunk_consumptions)]
+    next_consumption_price = consumption_prices[0]
+
+    if chunk_total_consumption >= soc * np.sqrt(0.83):
+        # If fully discharging the battery doesn't bring the consumption to 0, we take the highest
+        # price*consumption value and bring it down to the next highest price*consumption by reducing the
+        # consumption at that time step. We do this consecutively until the battery has been emptied.
+
+        local_soc = soc * np.sqrt(0.83)
+        # print(local_soc)
+        chunk_charge_loads = [0] * len(chunk_consumptions)
+        # print(chunk_charge_loads)
+
+        while local_soc != 0:
+            max_consumption_price = max(consumption_prices)
+            peak_indices = [i for i, p in enumerate(consumption_prices) if p == max_consumption_price]
+
+            consumption_prices_without_peak = [x for x in consumption_prices if x != max_consumption_price]
+
+            if len(consumption_prices_without_peak) == 0:
+                consumption_prices_without_peak = [0]
+
+            difference_from_peak = max_consumption_price - max(consumption_prices_without_peak)
+            consumption_difference = [difference_from_peak / prices[i] for i in peak_indices]
+
+            if local_soc >= sum(consumption_difference):
+                for i, difference in enumerate(consumption_difference):
+                    chunk_charge_loads[peak_indices[i]] += difference
+                    local_soc -= difference
+                    consumption_prices[peak_indices[i]] -= difference_from_peak
+                    # print(chunk_charge_loads)
+            else:
+                relative_difference = [c / sum(consumption_difference) for c in consumption_difference]
+
+                for i, rd in enumerate(relative_difference):
+                    chunk_charge_loads[peak_indices[i]] += local_soc * rd
+                    consumption_prices[peak_indices[i]] -= rd * local_soc * prices[peak_indices[i]]
+                    # print(chunk_charge_loads)
+
+                local_soc = 0
+
+        return chunk_charge_loads
+    else:
+        return chunk_consumptions
+
+
+def calculate_next_chunk(observation, consumption_sign, agent_id, timestep, remaining_battery_capacity, soc):
+
     chunk_consumptions = get_chunk_consumptions(agent_id, timestep, consumption_sign)
     if consumption_sign == -1:  # If negative consumption
         chunk_charge_loads = negative_consumption_scenario(chunk_consumptions, remaining_battery_capacity, soc)
     else:
-        chunk_charge_loads = chunk_consumptions
+        chunk_charge_loads = positive_consumption_scenario(observation, chunk_consumptions, timestep, remaining_battery_capacity, soc)
+
     return chunk_charge_loads
 
 
@@ -64,15 +124,15 @@ def individual_consumption_policy(observation, timestep, agent_id, remaining_bat
     if next_consumption == 0:
         return 0
 
-    if next_consumption > 0:
+    elif next_consumption > 0:
         consumption_sign = 1
     else:
         consumption_sign = -1
 
-    chunk_charge_loads = calculate_next_chunk(consumption_sign, agent_id, timestep, remaining_battery_capacity,
+    chunk_charge_loads = calculate_next_chunk(observation, consumption_sign, agent_id, timestep, remaining_battery_capacity,
                                               soc)
-    consumption_sign = -consumption_sign
 
+    # print(chunk_charge_loads)
 
     charge_load = -1 * consumption_sign * chunk_charge_loads[0]
     action = charge_load / remaining_battery_capacity
@@ -80,7 +140,8 @@ def individual_consumption_policy(observation, timestep, agent_id, remaining_bat
     return action
 
 
-class KnownConsumptionAgentTime:
+
+class KnownConsumptionAgentPeak:
 
     def __init__(self):
         self.action_space = {}
@@ -104,11 +165,14 @@ class KnownConsumptionAgentTime:
                                           self.remaining_battery_capacity[agent_id],
                                           self.soc[agent_id])
 
-        energy = n.energy_normed(action_out * self.remaining_battery_capacity[agent_id], 5)
+        action = float(np.array(action_out, dtype=self.action_space[agent_id].dtype))
+        max_power = n.max_power(self.soc[agent_id], 5, self.remaining_battery_capacity[agent_id])
+        energy = n.energy_normed(action * self.remaining_battery_capacity[agent_id], max_power)
         efficiency = n.efficiency(energy, 5)
 
         previous_soc = self.soc[agent_id]
         self.soc[agent_id] = n.soc(energy, previous_soc, efficiency, self.remaining_battery_capacity[agent_id])
+        print(self.soc[agent_id])
 
         battery_cons = n.last_energy_balance(self.soc[agent_id], previous_soc, efficiency)
         self.remaining_battery_capacity[agent_id] = n.new_capacity(self.remaining_battery_capacity[agent_id],
--- agents/user_agent.py
+++ agents/user_agent.py
@@ -3,6 +3,7 @@
 ###################################################################
 from agents.known_consumption_agent import KnownConsumptionAgent
 from agents.pred_consumption_agent import PredConsumptionAgent
-from agents.known_consumption_agent_timestep import KnownConsumptionAgentTime
+from agents.known_consumption_agent_peak import KnownConsumptionAgentPeak
+# from agents.unrefactored_known_consumption_timestep import TimeStepKnownConsumptionAgent
 
-UserAgent = KnownConsumptionAgentTime
+UserAgent = KnownConsumptionAgentPeak
