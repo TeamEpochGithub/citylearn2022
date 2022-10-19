3a8d25af31bad3c3e4e49198a466e057410c00b2
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-19 16:16:44 +0200

    add timestep pred consumption agent

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,21 +1,9 @@
-#from agents.individual_consumption_agent import IndividualConsumptionAgent
-from agents.improved_individual_consumption import ImprovedIndividualConsumptionAgent
-from agents.known_consumption_agent import KnownConsumptionAgent
-from agents.improved_individual_consumption import ImprovedIndividualConsumptionAgent
+
 from agents.timestep_known_consumption_agent import TimeStepKnownConsumptionAgent
+from agents.timestep_pred_consumption_agent import TimeStepPredConsumptionAgent
 
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
 
-# UserAgent = RandomAgent
-# UserAgent = KnownConsumptionAgent
-# UserAgent = ImprovedIndividualConsumptionAgent
-UserAgent = TimeStepKnownConsumptionAgent
-# UserAgent = SAC(RLCAgent)
-# UserAgent = BasicRewardAgent
-# UserAgent = BasicWeatherAgent
-#UserAgent = ConsumptionBasedAgent
-# UserAgent = DayTunedAgent
-
-# Changen naar MonthTunedAgent en dan weer submission maken
+UserAgent = TimeStepPredConsumptionAgent
