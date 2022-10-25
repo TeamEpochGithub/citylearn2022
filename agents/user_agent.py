247ae23ac44753dffbbff167b9a43174899dfeca
Lars <l.w.vanblokland@student.tudelft.nl>
2022-10-25 16:12:00 +0200

    User Agent change

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -2,7 +2,8 @@
 #####                Specify your agent here                  #####
 ###################################################################
 from agents.timestep_known_consumption_agent_peak import TimeStepKnownConsumptionAgentPeak
+from agents.timestep_pred_consumption_agent_peak_testing import TimeStepPredConsumptionAgentPeakTesting
 from agents.timestep_pred_consumption_agent_peak import TimeStepPredConsumptionAgentPeak
 
-UserAgent = TimeStepKnownConsumptionAgentPeak
+UserAgent = TimeStepPredConsumptionAgentPeakTesting
 
