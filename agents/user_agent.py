5d1a27e449a2a84826319956687daf1ef9f2eb67
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-27 11:30:33 +0200

    refactor

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,9 +1,9 @@
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
-from agents.contextual_bandit import ContextualBanditAgent
 from agents.timestep_known_consumption_agent_peak import TimeStepKnownConsumptionAgentPeak
 from agents.timestep_pred_consumption_agent_peak import TimeStepPredConsumptionAgentPeak
+from agents.vowpal_wabbit.contextual_bandit import ContextualBanditAgent
 
 UserAgent = ContextualBanditAgent
 
