22c3524be66a68b8afd75b5a2f7cc5319477f255
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-28 15:44:07 +0200

    revert first solar learner to original scaler

--- agents/tunable_agents/pick_tuning_agent.py
+++ agents/tunable_agents/pick_tuning_agent.py
@@ -1,5 +1,4 @@
 from agents.tunable_agents.tunable_original_pred_peak import TunableOriginalPredPeakAgent
-from agents.tunable_agents.tunable_timestep_pred_consumption_agent_peak import TunableTimeStepPredConsumptionAgentPeak
 
 ###################################################################
 #####                Specify your agent here                  #####
--- analysis/experiments/hour_nudges_multiprocessing.py
+++ analysis/experiments/hour_nudges_multiprocessing.py
@@ -1,41 +1,40 @@
-search_space = {
-    "hour_0": range(1, 25),
-    # "hour_1": range(1, 25),
-    # "hour_2": range(1, 25),
-    # "hour_3": range(1, 25),
-    "action_0": [x / 50.0 for x in range(-20, 20, 1)[15:25]],
-    # "action_1": [x / 50.0 for x in range(-20, 20, 1)[15:25]],
-    # "action_2": [x / 50.0 for x in range(-20, 20, 1)[15:25]],
-    # "action_3": [x / 50.0 for x in range(-20, 20, 1)[15:25]],
-}
-print(search_space)
-
 from traineval.evaluation.tune_evaluation import evaluate
-
-hours = range(1, 25)
-actions = [x / 50.0 for x in range(-20, 20, 1)[15:25]]
-
 from multiprocessing import Pool, cpu_count
 
+
+hours = range(1, 25)
+actions = [x / 100.0 for x in range(-15, 15, 3)]
+
+print(str(hours))
+print(str(actions))
+
+def get_all_pairs():
+    pairs = []
+    for i in range(len(hours)):
+        for j in range(len(actions)):
+            pairs.append((i, j))
+    return pairs
+
+
+def get_params(all_pairs):
+    all_params = []
+    for p in all_pairs:
+        all_params.append({"hour": hours[p[0]], "action": actions[p[1]]})
+    return all_params
+
+
 if __name__ == '__main__':
-    #
-    # all_possibilities = []
-    # for i in range(len(hours)):
-    #     for j in range(len(actions)):
-    #         params = {"hour_0": hours[i],
-    #                   "action_0": actions[j]}
-    #         all_possibilities.append(params)
-
     all_scores = {}
-
-    with Pool(processes=(cpu_count() - 6)) as pool:
-        for i in range(len(hours)):
-            for j in range(len(actions)):
-                res = pool.apply_async(evaluate, args=({"hour_0": hours[i], "action_0": actions[j]},))
-                print(res.get())
-                loss, params = res.get()
-                all_scores[str(params)] = loss
+    all_pairs = get_all_pairs()
+    all_params = get_params(all_pairs)
+    with Pool(processes=(cpu_count() - 4)) as pool:
+        res = pool.map_async(evaluate, all_params)
+        print(res.get())
+        loss, params = res.get()
+        all_scores[str(params)] = loss
 
     pool.close()
     pool.join()
+    print(all_scores)
+    all_scores = {k: v for k, v in sorted(all_scores.items(), key=lambda item: item[1])}
     print(all_scores)
