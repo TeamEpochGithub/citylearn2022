65109a1c2124ac8769da5bee4da0c7d0b486dbb8
Philip Tempelman <tempelmanphilip@gmail.com>
2022-10-28 10:51:16 +0200

    select other agent

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,7 +1,8 @@
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
+from agents.district_pred_consumption_agent import DistrictPredConsumptionAgent
 from agents.original_pred_peak import OriginalPredPeakAgent
 
-UserAgent = OriginalPredPeakAgent
+UserAgent = DistrictPredConsumptionAgent
 
