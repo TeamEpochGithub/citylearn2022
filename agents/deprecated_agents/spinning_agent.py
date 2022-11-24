7b52c25219bc1a72fe7d408dd75eb2598e4aab68
Lionel <76404652+quantagenmtg@users.noreply.github.com>
2022-11-24 10:24:15 +0000

    remove deprecated agents

--- agents/deprecated_agents/spinning_agent.py
+++
@@ -1,7 +0,0 @@
-from agents.deprecated_agents.custom_agent import CustomDRLAgent
-
-###################################################################
-#####                Specify your agent here                  #####
-###################################################################
-
-SpinningAgent = CustomDRLAgent
--- agents/tunable_agents/pick_tuning_agent.py
+++
@@ -1,7 +0,0 @@
-from agents.tunable_agents.tunable_original_pred_peak import TunableOriginalPredPeakAgent
-
-###################################################################
-#####                Specify your agent here                  #####
-###################################################################
-
-TuningAgent = TunableOriginalPredPeakAgent
