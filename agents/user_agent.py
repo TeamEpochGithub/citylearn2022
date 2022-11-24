5624a3af9247fdfaf0175356e200cc034c45e5e4
Lionel <76404652+quantagenmtg@users.noreply.github.com>
2022-11-24 13:57:10 +0000

    Remove files not used in final agent

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,8 +1,6 @@
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
-from agents.district_known_consumption_agent import DistrictKnownConsumptionAgent
-from agents.district_pred_consumption_agent import DistrictPredConsumptionAgent
 from agents.original_pred_peak import OriginalPredPeakAgent
 
 UserAgent = OriginalPredPeakAgent
